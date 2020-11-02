cee379e70bf32e3d7ecd62210b6c03dd850e6d86
Janne Kemppainen <ext-janne.k.kemppainen@elisa.fi>
2020-11-02 16:33:05 +0200

    log release data

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const core = require("@actions/core");
 const { getOctokit, context } = require("@actions/github");
 
 async function run() {
+    const tag = core.getInput("tag") || context.payload.inputs.tag;
     const payload = context.payload;
-    console.log(payload);
+    const token = core.getInput("github_token", { required: true });
+    const octokit = getOctokit(token);
+    const { owner, repo } = context.repo;
+    const release = await octokit.repos.getReleaseByTag({ owner, repo, tag });
+    console.log(release);
 }
 
 run();
